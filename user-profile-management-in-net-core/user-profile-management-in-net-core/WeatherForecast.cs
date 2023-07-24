bbc98b49fc801206d6bceeb4e43d620fecfea1f8
Ravi Koppula <ravikoppula92@gmail.com>
2023-07-24 13:54:33 +0800

    Initial Commit

---
+++ user-profile-management-in-net-core/user-profile-management-in-net-core/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace user_profile_management_in_net_core
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
